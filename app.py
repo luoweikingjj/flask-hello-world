5ce1e2592edce2a35ef4688ced3ecf80a86c2b41
luoweikingjj <luoweikingjj@gmail.com>
2020-09-10 11:26:40 +0000

    update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "hello docker, version 1.0"
+    return "hello docker, version 1.0!"
 if __name__ == '__main__':
     app.run()
